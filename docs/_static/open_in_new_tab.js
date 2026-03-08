f824e5adadb2a76e748ce7f9dd23025a3f97bf18
Peter Kammel <pkammel@uw.edu>
2026-03-07 21:09:09 -0800

    Cleanup: Remove documentation folders from physics repo

--- docs/_static/open_in_new_tab.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Select all external links
-  const links = document.querySelectorAll("a.external");
-
-  links.forEach(function (link) {
-    link.setAttribute("target", "_blank");
-    link.setAttribute("rel", "noopener noreferrer");
-  });
-});
